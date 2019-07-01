744a5351d0660749ae81be6867a82b283f4c1b61
cgrossi <cgrossi@gmail.com>
2019-06-30 22:05:18 -0600

    Add button styling

--- main.js
+++ main.js
@@ -3,174 +3,176 @@ const grid = document.querySelector('.game-board');
 const squares = document.querySelectorAll('.square');
 const pveButton = document.querySelector('.pve');
 const pvpButton = document.querySelector('.pvp');
-const reset = document.querySelector('.reset')
+const reset = document.querySelector('.reset');
 
-
-
-const gameBoard = (() => {
-    return {
+const gameBoard = (() => ({
         currentTurn: null,
         gameType: null,
         board: [],
         winner: null,
-        gameStart(type){
-            this.currentTurn = 'X'
-            if(this.gameType == 'pvp') {
-                squares.forEach((el, index) => {
-                    el.classList.add(`s${index + 1}`)
-                    el.addEventListener('click', (event) => {
-                        if(!event.target.textContent && this.currentTurn !== null) {
-                            event.target.textContent = this.currentTurn;
-                            this.board[index] = this.currentTurn;
-                            this.checkWin(this.currentTurn)
-                            if(this.winner == null) {
-                                this.switchTurn();
-                            } else {
-                                output.textContent = `The winner is ${this.winner}!`
-                            }
-                        } else if (!this.currentTurn) {
-                            this.currentTurn = 'X'
-                            event.target.textContent = this.currentTurn;
-                            this.board[index] = this.currentTurn;
-                            this.checkWin(this.currentTurn)
-                            console.log(this.currentTurn)
-                            if(this.winner == null) {
-                                this.switchTurn();
-                            } else {
-                                output.textContent = `The winner is ${this.winner}!`
-                            }
-                            
-                        }
-                    })
-                }) 
-            } else if(this.gameType == 'pve') {
-                console.log('in pve')
-                // Set square event listeners & manage player turn
-                squares.forEach((el, index) => {
-                    el.classList.add(`s${index + 1}`)
-                    })
-                    squares.forEach((el,index) => {
-                        el.addEventListener('click', (event) => {
-                            if(!event.target.textContent && this.currentTurn == 'X') {
-                                event.target.textContent = this.currentTurn;
-                                this.board[index] = this.currentTurn;
-                                this.checkWin(this.currentTurn)
-                                if(this.winner == null) {
-                                    this.switchTurn();
-                                    const that = this
-                                    setTimeout(function cpu() {
-                                        let cpuPick = Math.floor(Math.random() * 9)
-                                        if(that.board[cpuPick] == undefined) {
-                                            that.board[cpuPick] = 'O'
-                                            document.querySelector(`.s${cpuPick + 1}`).textContent = 'O'
-                                            that.checkWin(that.currentTurn)
-                                            if(that.winner == null) {
-                                                that.switchTurn()
-                                            } else {
-                                                output.textContent = `The winner is ${that.winner}!`
-                                            }
-                                        } else {
-                                            let force = 0
-                                            while(force < 9) {
-                                                if(that.board[force] == undefined && that.currentTurn == 'O') {
-                                                    that.board[force] = 'O'
-                                                    document.querySelector(`.s${force + 1}`).textContent = 'O'
-                                                    that.checkWin(that.currentTurn)
-                                                    if(that.winner == null) {
-                                                    that.switchTurn()
-                                                    break;
-                                                    } else {
-                                                        output.textContent = `The winner is ${that.winner}!`
-                                                        break;
-                                                    }
+        gameStart(type) {
+                this.currentTurn = 'X';
+                if (this.gameType == 'pvp') {
+                        squares.forEach((el, index) => {
+                                el.classList.add(`s${index + 1}`);
+                                el.addEventListener('click', event => {
+                                        if (!event.target.textContent && this.currentTurn !== null) {
+                                                event.target.textContent = this.currentTurn;
+                                                this.board[index] = this.currentTurn;
+                                                this.checkWin(this.currentTurn);
+                                                if (this.winner == null) {
+                                                        this.switchTurn();
+                                                } else {
+                                                        output.textContent = `The winner is ${this.winner}!`;
                                                 }
-                                                force++
-                                            }
+                                        } else if (!this.currentTurn) {
+                                                this.currentTurn = 'X';
+                                                event.target.textContent = this.currentTurn;
+                                                this.board[index] = this.currentTurn;
+                                                this.checkWin(this.currentTurn);
+                                                console.log(this.currentTurn);
+                                                if (this.winner == null) {
+                                                        this.switchTurn();
+                                                } else {
+                                                        output.textContent = `The winner is ${this.winner}!`;
+                                                }
                                         }
-    
-                                    }, 1000)
-                                } else {
-                                    output.textContent = `The winner is ${this.winner}!`
-                                }
-                            } 
-                        })
-                    })
-            }
+                                });
+                        });
+                } else if (this.gameType == 'pve') {
+                        console.log('in pve');
+                        // Set square event listeners & manage player turn
+                        squares.forEach((el, index) => {
+                                el.classList.add(`s${index + 1}`);
+                        });
+                        squares.forEach((el, index) => {
+                                el.addEventListener('click', event => {
+                                        if (!event.target.textContent && this.currentTurn == 'X') {
+                                                event.target.textContent = this.currentTurn;
+                                                this.board[index] = this.currentTurn;
+                                                this.checkWin(this.currentTurn);
+                                                if (this.winner == null) {
+                                                        this.switchTurn();
+                                                        const that = this;
+                                                        setTimeout(function cpu() {
+                                        const cpuPick = Math.floor(Math.random() * 9)
+                                                                if (that.board[cpuPick] == undefined) {
+                                                                        that.board[cpuPick] = 'O';
+                                                                        document.querySelector(
+                                                                                `.s${cpuPick + 1}`
+                                                                        ).textContent = 'O';
+                                                                        that.checkWin(that.currentTurn);
+                                                                        if (that.winner == null) {
+                                                                                that.switchTurn();
+                                                                        } else {
+                                                                                output.textContent = `The winner is ${
+                                                                                        that.winner
+                                                                                }!`;
+                                                                        }
+                                                                } else {
+                                                                        let force = 0;
+                                                                        while (force < 9) {
+                                                                                if (
+                                                                                        that.board[force] ==
+                                                                                                undefined &&
+                                                                                        that.currentTurn == 'O'
+                                                                                ) {
+                                                                                        that.board[force] = 'O';
+                                                                                        document.querySelector(
+                                                                                                `.s${force + 1}`
+                                                                                        ).textContent = 'O';
+                                                                                        that.checkWin(that.currentTurn);
+                                                                                        if (that.winner == null) {
+                                                                                                that.switchTurn();
+                                                                                                break;
+                                                                                        } else {
+                                                                                                output.textContent = `The winner is ${
+                                                                                                        that.winner
+                                                                                                }!`;
+                                                                                                break;
+                                                                                        }
+                                                                                }
+                                                                                force++;
+                                                                        }
+                                                                }
+                                                        }, 1000);
+                                                } else {
+                                                        output.textContent = `The winner is ${this.winner}!`;
+                                                }
+                                        }
+                                });
+                        });
+                }
         },
-        switchTurn(){
-            console.log('switch turns')
-            if(this.currentTurn == 'X') {
-                this.currentTurn = 'O'
-            } else {
-                this.currentTurn = 'X'
-            }
+        switchTurn() {
+                console.log('switch turns');
+                if (this.currentTurn == 'X') {
+                        this.currentTurn = 'O';
+                } else {
+                        this.currentTurn = 'X';
+                }
         },
         checkWin(marker) {
-            console.log('check win')
-            if(!this.board.includes(undefined)) {
-                output.textContent = `It's a tie!!`
-            }
-            if(this.board[0] == marker && this.board[1] == marker && this.board[2] == marker) {
-                this.winner = marker
-            }
-            if(this.board[3] == marker && this.board[4] == marker && this.board[5] == marker) {
-                this.winner = marker
-            }
-            if(this.board[6] == marker && this.board[7] == marker && this.board[8] == marker) {
-                this.winner = marker
-            }
-            if(this.board[0] == marker && this.board[3] == marker && this.board[6] == marker) {
-                this.winner = marker
-            }
-            if(this.board[1] == marker && this.board[4] == marker && this.board[7] == marker) {
-                this.winner = marker
-            }
-            if(this.board[2] == marker && this.board[5] == marker && this.board[8] == marker) {
-                this.winner = marker
-            }
-            if(this.board[0] == marker && this.board[4] == marker && this.board[8] == marker) {
-                this.winner = marker
-            }
-            if(this.board[2] == marker && this.board[4] == marker && this.board[6] == marker) {
-                this.winner = marker
-            }
+                console.log('check win');
+                if (!this.board.includes(undefined)) {
+                        output.textContent = `It's a tie!!`;
+                }
+                if (this.board[0] == marker && this.board[1] == marker && this.board[2] == marker) {
+                        this.winner = marker;
+                }
+                if (this.board[3] == marker && this.board[4] == marker && this.board[5] == marker) {
+                        this.winner = marker;
+                }
+                if (this.board[6] == marker && this.board[7] == marker && this.board[8] == marker) {
+                        this.winner = marker;
+                }
+                if (this.board[0] == marker && this.board[3] == marker && this.board[6] == marker) {
+                        this.winner = marker;
+                }
+                if (this.board[1] == marker && this.board[4] == marker && this.board[7] == marker) {
+                        this.winner = marker;
+                }
+                if (this.board[2] == marker && this.board[5] == marker && this.board[8] == marker) {
+                        this.winner = marker;
+                }
+                if (this.board[0] == marker && this.board[4] == marker && this.board[8] == marker) {
+                        this.winner = marker;
+                }
+                if (this.board[2] == marker && this.board[4] == marker && this.board[6] == marker) {
+                        this.winner = marker;
+                }
         },
-        resetGame(){
-            this.winner = null
-            this.board = []
-            this.currentTurn = null
-            squares.forEach(el => {
-                el.textContent = ''
-            })
-            output.textContent = ''
-            this.gameStart()
-        }
-    }
-})();
+        resetGame() {
+                this.winner = null;
+                this.board = [];
+                this.currentTurn = null;
+                squares.forEach(el => {
+                        el.textContent = '';
+                });
+                output.textContent = '';
+                this.gameStart();
+        },
+}))();
 
 function showGame() {
-    output.classList.remove('hide')
-    reset.classList.remove('hide')
-    grid.classList.remove('hide')
-    pveButton.classList.add('hide')
-    pvpButton.classList.add('hide')
+        output.classList.remove('hide');
+        reset.classList.remove('hide');
+        grid.classList.remove('hide');
+        pveButton.classList.add('hide');
+        pvpButton.classList.add('hide');
 }
 
 pveButton.addEventListener('click', () => {
-    gameBoard.gameType = 'pve'
-    showGame();
-    gameBoard.gameStart('pve')
-})
+        gameBoard.gameType = 'pve';
+        showGame();
+        gameBoard.gameStart('pve');
+});
 
 pvpButton.addEventListener('click', () => {
-    gameBoard.gameType = 'pvp'
-    showGame();
-    gameBoard.gameStart('pvp')
-
-})
+        gameBoard.gameType = 'pvp';
+        showGame();
+        gameBoard.gameStart('pvp');
+});
 reset.addEventListener('click', () => {
-    gameBoard.resetGame()
-})
-
-
-
+        gameBoard.resetGame();
+});
